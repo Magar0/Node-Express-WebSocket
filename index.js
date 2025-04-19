8887379600a3f1328bfdbf2f6831a8bc45aa7d90
Magar0 <rakeshmagarr0@gmail.com>
2025-04-19 22:37:04 +0545

    done

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// socket.io
+io.on("connection", (socket) => {
+  socket.on("chat message", (message) => io.emit("message", message));
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
+});
+
+server.listen(9000, () => console.log("server started at Port: 9000"));
