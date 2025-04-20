dfb1dda14869f6d742a5019c5a82d62120df7ec4
Magar0 <rakeshmagarr0@gmail.com>
2025-04-20 15:18:30 +0545

    updated chat app

--- index.js
+++ index.js
@@ -7,9 +7,24 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const users = {}; //store users name
+
 // socket.io
 io.on("connection", (socket) => {
-  socket.on("chat message", (message) => io.emit("message", message));
+  socket.on("new-user", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-connected", name);
+  });
+  socket.on("send-chat-message", (message) => {
+    // io.emit("message", message) //to send all socket
+    socket.broadcast.emit("chat-message", { message, name: users[socket.id] });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconened");
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
+  });
 });
 
 app.use(express.static(path.resolve("./public")));
